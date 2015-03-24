7f971530543de89ed86cae8df2f82d7550c0e1ff
gduck <dale.github@thefoos.net>
2015-03-24 17:33:00 +0800

    ok for now la

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,26 +4,50 @@ angular.module('yeoTestApp')
   .controller('MainCtrl', function ($scope, $http, socket) {
     $scope.awesomeThings = [];
 
+    var $container = $('#packery-container');
+    // init
+    $container.packery({
+      itemSelector: '.cell'
+      // gutter: 10
+    });
+
     $http.get('/api/things').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
       socket.syncUpdates('thing', $scope.awesomeThings);
     });
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
-
-    $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
+    $scope.$container = $('#packery-container');
+    $scope.repack = function() {
+      $scope.$container.packery();
+      return true;
+    }
+  
+    $('#packery-container').imagesLoaded( function() {
+      $scope.repack();
     });
+
+    // $scope.$container.find('.cell').each( function( i, itemElem ) {
+    //   // make element draggable with Draggabilly
+    //   var draggie = new Draggabilly( itemElem );
+    //   // bind Draggabilly events to Packery
+    //   $scope.$container.packery( 'bindDraggabillyEvents', draggie );
+    // });
+
+    // $scope.addThing = function() {
+    //   if($scope.newThing === '') {
+    //     return;
+    //   }
+    //   $http.post('/api/things', { name: $scope.newThing });
+    //   $scope.newThing = '';
+    // };
+
+    // $scope.deleteThing = function(thing) {
+    //   $http.delete('/api/things/' + thing._id);
+    // };
+
+    // $scope.$on('$destroy', function () {
+    //   socket.unsyncUpdates('thing');
+    // });
 
     var project = $('.project');
     console.log(project);
